6943d6deb15dc038ccb69b808e0675c8dfddd244
Melissa Zhang <melissazhangyz@outlook.com>
2024-04-04 22:48:48 -0400

    quiz12

---
+++ quiz12b/Untitled.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+
+set.seed(123)
+
+n <- 1000
+
+data <- tibble( Support = rbinom(n, 1, 0.5),
+                
+                AgeGroup = sample(c('18-24', '25-34', '35-44', '45-54', '55-64', '65+'), n, replace = TRUE),
+                
+                Gender = sample(c('Male', 'Female', 'Other'), n, replace = TRUE),
+                
+                IncomeGroup = sample(c('Low', 'Middle', 'High'), n, replace = TRUE),
+                
+                HighestEducation = sample(c('Less than High School', 'High School Graduate', 
+                                            'Some College', 'Bachelor Degree', 'Graduate Degree'), 
+                                          n, replace = TRUE) )
+
+
+#tests
+data %>%
+  group_by(AgeGroup) %>%
+  summarize(SupportYes = mean(Support == 1))
+
+data %>%
+  group_by(Gender) %>%
+  summarize(SupportYes = mean(Support == 1))
+
+data %>%
+  group_by(IncomeGroup) %>%
+  summarize(SupportYes = mean(Support == 1))
+
+data %>%
+  group_by(HighestEducation) %>%
+  summarize(SupportYes = mean(Support == 1))
+
+data %>%
+  group_by(AgeGroup, Gender) %>%
+  summarize(SupportYes = mean(Support == 1))
+
+data %>%
+  group_by(IncomeGroup, HighestEducation) %>%
+  summarize(SupportYes = mean(Support == 1))
